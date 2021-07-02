855f93c83a428a4c150b97aca0663f90e6d8d262
harsh1234567 <ariharsh1994@gmail.com>
2021-07-02 13:54:35 +0530

    Update TernaryTrie.java
    add changes in ternary trie.

--- src/main/java/Trie/TernaryTrie.java
+++ src/main/java/Trie/TernaryTrie.java
@@ -32,21 +32,6 @@ class Pair {
 
 }
 
-class Node {
-	Node left, mid, right = null;
-	char val;
-	boolean isEnd;
-	int freq;
-	int max;
-
-	public Node() {
-	}
-
-	public Node(char val) {
-		this.val = val;
-	}
-}
-
 public class TernaryTrie {
 	private Node root;
 
@@ -59,35 +44,17 @@ public class TernaryTrie {
 			root = insert(word, 0, root);
 	}
 
-	private int maxFreq(Node a) {
-		int maxi = 0;
-		if (a.mid == null) {
-			maxi = a.freq;
-		} else {
-			if (a.mid != null) {
-				maxi = Math.max(a.max, a.mid.max);
-			}
-			if (a.mid != null && a.mid.left != null) {
-				maxi = Math.max(a.max, a.mid.left.max);
-			}
-			if (a.mid != null && a.mid.right != null) {
-				maxi = Math.max(a.max, a.mid.right.max);
-			}
-		}
-		return maxi;
-	}
 
+	// insert  character along with frequency of the character which is
+	// updated on every insertion .
 	private Node insert(String word, int index, Node node) {
 		char ch = word.charAt(index);
 		if (node == null) {
 			node = new Node(ch);
-			if (index == word.length() - 1) {
-				node.freq = 1;
-			} else {
-				node.freq = 0;
-			}
+			node.freq = 0;
+//			System.out.print(node.val+"   ");
+		//	System.out.print(node.freq+"   ");
 		}
-//		System.out.println(node.val);
 
 		if (ch < node.val) {
 			node.left = insert(word, index, node.left);
@@ -99,17 +66,20 @@ public class TernaryTrie {
 				node.mid = insert(word, index + 1, node.mid);
 			} else {
 				node.freq++;
+//
+//				System.out.print(node.val+"   ");
+//				System.out.print(node.freq+"   ");
+//				System.out.print(index);
 			}
-
 		}
-		node.max = maxFreq(node);
+//		node.max = maxFreq(node);
 		return node;
 	}
 
+//search based on prefix of word it will give list of sentences or word if present otherwise it will give not found result  
 	private Queue<Pair> search(String word) {
 		Queue<Pair> words = new PriorityQueue<Pair>(Comparator.comparing(Pair::getFreq).reversed());
 		Node node = startsWith(word, 0, root);
-//		System.out.println(node.val);
 		startsWith(words, "", word.substring(0, word.length() - 1), node);
 		return words;
 	}
@@ -120,9 +90,9 @@ public class TernaryTrie {
 			return null;
 		}
 		if (prefix.charAt(index) < node.val) {
-			return startsWith(prefix, index + 1, node.left);
+			return startsWith(prefix, index, node.left);
 		} else if (prefix.charAt(index) > node.val) {
-			return startsWith(prefix, index + 1, node.right);
+			return startsWith(prefix, index, node.right);
 		} else {
 			if (index == prefix.length() - 1) {
 				return node;
@@ -145,30 +115,46 @@ public class TernaryTrie {
 			startsWith(wordList, temp, prefix, node.mid);
 		}
 		if (node.mid != null && node.mid.left != null) {
-
 			startsWith(wordList, temp, prefix, node.mid.left);
-
 		}
 
 		if (node.mid != null && node.mid.right != null) {
-
 			startsWith(wordList, temp, prefix, node.mid.right);
 		}
 
 		temp = temp.substring(0, temp.length() - 1);
 	}
 
-	public static void main(String[] args) {
-		String st[] = { "acco", "accolite", "accol", "aa" };
-		TernaryTrie ternaryTrie = new TernaryTrie();
-		for (String word : st) {
-			ternaryTrie.insert(word);
+	public void insertAll(String[] words) {
+		for(String w:words) {
+		  insert(w);
 		}
-		Queue<Pair> words = ternaryTrie.search("a");
-		System.out.println(words.size());
-		while (!words.isEmpty()) {
-			Pair p = words.poll();
+		//Arrays.asList(words).stream().forEach(word -> insert(word));
+	}
+
+	public static void main(String[] args) {
+		TernaryTrie ternaryTrie = new TernaryTrie();
+		
+		String words[] = { "harry potter", "harry", "potter", "harry" };
+		ternaryTrie.insertAll(words);
+		
+		test(words,"h",ternaryTrie);
+		System.out.println("test 2------------------");
+	//	test(words,"p",ternaryTrie);
+
+		test(words,"p",ternaryTrie);
+
+	}
+
+	
+
+	private static void test(String[] words,String prefix,TernaryTrie ternaryTrie) {
+		// type head search based on top k frequency .
+		Queue<Pair> wordList = ternaryTrie.search(prefix);
+		while (!wordList.isEmpty()) {
+			Pair p = wordList.poll();
 			System.out.println(p.word + "," + p.freq);
 		}
 	}
+
 }
